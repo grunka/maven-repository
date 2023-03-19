30ea320905f92a4da80c1a91a8c3e213c6e84e19
John Wästerlund <grunka@gmail.com>
2023-03-19 12:06:10 +0100

    Adding some thoughts about user configuration

--- src/main/java/com/grunka/maven/MavenRepositoryConfiguration.java
+++ src/main/java/com/grunka/maven/MavenRepositoryConfiguration.java
@@ -10,4 +10,12 @@ public class MavenRepositoryConfiguration extends Configuration {
     public String storageDirectory;
     @NotNull
     public Map<String, String> remoteRepositories;
+
+    //TODO authentication configuration?
+    // users:
+    //   <username>: <password>
+    // access:
+    //   <username>:
+    //     read: <repo>
+    //     write: <repo>
 }
