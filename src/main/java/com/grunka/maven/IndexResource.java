0d055c4845b522f3fb46b9d63597faa118b0ff38
John Wästerlund <grunka@gmail.com>
2023-03-26 18:38:29 +0200

    Adding cache

--- src/main/java/com/grunka/maven/IndexResource.java
+++ src/main/java/com/grunka/maven/IndexResource.java
@@ -1,39 +1,52 @@
 package com.grunka.maven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Path("/")
 public class IndexResource {
+    private static final Logger LOG = LoggerFactory.getLogger(IndexResource.class);
+    private final Map<String, Optional<byte[]>> lazyLoadCache = new ConcurrentHashMap<>();
+    private Optional<byte[]> getBytes(String resource) {
+        return lazyLoadCache.computeIfAbsent(resource, r -> {
+            try (InputStream resourceAsStream = getClass().getResourceAsStream(r)) {
+                if (resourceAsStream == null) {
+                    return Optional.empty();
+                }
+                return Optional.of(resourceAsStream.readAllBytes());
+            } catch (IOException e) {
+                LOG.error("Failed to read resource {}", r, e);
+                return Optional.empty();
+            }
+
+        });
+    }
+
     @GET
     @Path("/")
     public Response indexHtml() {
-        try (InputStream resourceAsStream = getClass().getResourceAsStream("/index.html")) {
-            byte[] pomBytes = resourceAsStream.readAllBytes();
-            return Response
-                    .ok(pomBytes)
-                    .type(MediaType.TEXT_HTML_TYPE)
-                    .build();
-        } catch (IOException e) {
-            return Response.serverError().entity(e.getMessage()).build();
-        }
+        return getBytes("/index.html")
+                .map(b -> Response.ok(b).type(MediaType.TEXT_HTML_TYPE).build())
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
     }
 
     @GET
     @Path("/pom.png")
     public Response pomPng() {
-        try (InputStream resourceAsStream = getClass().getResourceAsStream("/pom.png")) {
-            byte[] pomBytes = resourceAsStream.readAllBytes();
-            return Response
-                    .ok(pomBytes)
-                    .type("img/png")
-                    .build();
-        } catch (IOException e) {
-            return Response.serverError().entity(e.getMessage()).build();
-        }
+        List<String> poms = List.of("/pom.png", "/pom2.png");
+        return getBytes(poms.get((int) Math.round(Math.random() * (poms.size() - 1))))
+                .map(b -> Response.ok(b).type("image/png").build())
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
     }
 }
